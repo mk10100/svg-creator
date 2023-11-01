3daee29529eb3a35b95cb7506d5370094c8bbcb8
mk10100 <mohamedkhalil10100@gmail.com>
2023-11-01 19:54:50 -0400

    working svg generator

---
+++ index.mjs
@@ -0,0 +1,66 @@
+import fs from "fs";
+import inquirer from "inquirer";
+import { Square, Triangle, Circle } from "./lib/shapes.mjs";
+
+const shapeQuestions = [
+  {
+    type: "list",
+    name: "shapeType",
+    message: "Select a shape:",
+    choices: ["triangle", "square", "circle"],
+  },
+  {
+    type: "input",
+    name: "textColor",
+    message: "Enter text color:",
+  },
+  {
+    type: "input",
+    name: "shapeColor",
+    message: "Enter shape color:",
+  },
+  {
+    type: "input",
+    name: "text",
+    message: "Enter text:",
+  },
+];
+
+function writeToFile(fileName, data) {
+  fs.writeFileSync(fileName, data, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("svg has been successfully generated.");
+    }
+  });
+}
+
+async function promptUserForInput() {
+  const answers = await inquirer.prompt(shapeQuestions);
+  const { shapeType, textColor, shapeColor, text } = answers;
+
+  let shape;
+
+  switch (shapeType) {
+    case "triangle":
+      shape = new Triangle(textColor, shapeColor, text);
+      break;
+    case "square":
+      shape = new Square(textColor, shapeColor, text);
+      break;
+    case "circle":
+      shape = new Circle(textColor, shapeColor, text);
+      break;
+    default:
+      console.log(
+        "Invalid shape type. Please choose triangle, square, or circle."
+      );
+      process.exit(1);
+  }
+
+  const svgContent = shape.toSVG();
+  writeToFile(`./examples/${text}.svg`, svgContent);
+}
+
+promptUserForInput();
